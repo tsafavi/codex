8818cdaf134f36d5306408633113be187dc5955e
Tara Lynn Safavi <tsafavi@umich.edu>
2020-05-28 17:03:33 +0000

    Add --size flag for TC

--- scripts/tc.sh
+++ scripts/tc.sh
@@ -3,5 +3,5 @@ declare -a sizes=("s" "m")
 
 for size in "${sizes[@]}"; do
     models=$(find models/triple-classification/codex-${size}/*/*.pt)
-    python scripts/tc.py ${models}
+    python scripts/tc.py ${models} --size ${size}
 done
