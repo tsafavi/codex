86d4f36337004d6100ac933f26ace0f66fe74e96
Tara Lynn Safavi <tsafavi@umich.edu>
2020-05-28 22:42:50 +0000

    Add pretrained download links

---
+++ models/download_pretrained.py
@@ -0,0 +1,46 @@
+import argparse
+import os
+
+import pandas as pd
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        'size',
+        choices=['s', 'm', 'l'],
+        help='CoDEx dataset to download model(s)'
+    )
+
+    parser.add_argument(
+        'task',
+        choices=['triple-classification', 'link-prediction'],
+        help='Task to download model(s) for'
+    )
+
+    parser.add_argument(
+        'models',
+        nargs='+',
+        choices=['rescal', 'transe', 'complex', 'conve'],
+        help='Model(s) to download for this task'
+    )
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    df = pd.read_csv('pretrained.csv')
+    df = df[
+        (df['size'] == args.size) &
+        (df['task'] == args.task) &
+        (df['model'].isin(args.models))
+    ]
+
+    for model, link in zip(df['model'], df['link']):
+        dst = '{}/codex-{}/{}/checkpoint_best.pt'.format(
+            args.task, args.size, model)
+        os.system('curl -L {} -o {}'.format(link, dst))
+        print('Downloaded LibKGE checkpoint to', dst)
