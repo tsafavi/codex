adb579fd45b58780605a0a9223673140951c8e83
Tara Lynn Safavi <tsafavi@umich.edu>
2020-05-31 10:41:34 +0000

    Extract English only

--- extract.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-declare -a arr=("entities" "types")
-for outer in "${arr[@]}"; do
-    for inner in $(find data/${outer} -maxdepth 1 -mindepth 1 -type d); do
-        unzip ${inner}/extracts.zip -d ${inner}/
-    done
-done
---
+++ extract_en.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+code="en"  # change this to extract a diffferent language
+unzip data/entities/${code}/extracts.zip -d data/entities/${code}/
+unzip data/types/${code}/extracts.zip -d data/types/${code}/
