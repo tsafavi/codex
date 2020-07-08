3dddca246e4fb616cef251bafb32dac648e8eedb
Tara Safavi <tsafavi@umich.edu>
2020-07-08 21:58:31 +0000

    Batch evaluation for baseline

--- scripts/baseline.py
+++ scripts/baseline.py
@@ -1,7 +1,4 @@
-"""
-Compare an embedding model to a non-learning
-frequency baseline.
-"""
+"""Compare an embedding model to a "non-learning" frequency baseline."""
 import argparse
 import torch
 
@@ -38,28 +35,28 @@ def entity_frequency(spo, direction="o"):
     """
     :param spo: torch.Tensor of triples
     :param direction: 'o' for tail entities, 's' for head entities
-    :return: [(entity, frequency proportion)] for all triples
+    :return: [(head or tail entity, frequency proportion)] for all triples
     """
     idx = 2 if direction == "o" else 0
     entities = spo[:, idx].numpy()
     counts = pd.Series(entities).value_counts(normalize=True)
-    return [(val, prop) for val, prop in zip(counts.index, counts)]
+    return list(zip(counts.index, counts))
 
 
 def score_by_frequency(model, test_spo, direction="o"):
     """
     :param dataset: kge.dataset.Dataset
     :param test_spo: torch.Tensor of test triples
-    :return: scores of leave-one-out test triples
+    :return: scores of test triples using an entity frequency scoring baseline
     """
     train_spo = model.dataset.split("train")
     scores = torch.zeros(
-        [len(test_spo), model.dataset.num_entities()], dtype=torch.double
+        [len(test_spo), model.dataset.num_entities()], dtype=torch.double, device="cpu",
     )
 
     freq = {}
-    for i, (s, p, o) in enumerate(test_spo):
-        p = int(p)
+    for i, triple in enumerate(test_spo):
+        s, p, o = [triple[i].item() for i in range(3)]
         rel_idx = train_spo[:, 1] == p
 
         # get most-frequent entities for this relation in train
@@ -68,15 +65,15 @@ def score_by_frequency(model, test_spo, direction="o"):
 
         # filter out seen head/tail entities
         ent_idx = train_spo[:, 0] == s if direction == "o" else train_spo[:, 2] == o
-
         idx = 2 if direction == "o" else 0
-        obs = set(train_spo[rel_idx & ent_idx][:, idx])
-        pairs = [pair for pair in freq[p] if pair[0] not in obs]
+        seen_entities = set(train_spo[rel_idx & ent_idx][:, idx])
+        pairs = [pair for pair in freq[p] if pair[0] not in seen_entities]
 
         # score the remaining entities by relative frequency
         eids, eid_scores = zip(*pairs) if len(pairs) else zip(*freq[p])
-
-        scores[i, eids] = torch.tensor(eid_scores, dtype=torch.double)
+        eids = torch.tensor(eids, dtype=torch.long, device="cpu")
+        eid_scores = torch.tensor(eid_scores, dtype=torch.double, device="cpu")
+        scores[i, eids] = eid_scores
 
     return scores
 
@@ -102,7 +99,9 @@ def filter_false_negatives(scores, test_spo, all_spo, direction="o"):
     :return scores: scores for each subject or object in test,
         with false negatives filtered out
     """
-    for i, (s, p, o) in enumerate(test_spo):
+    for i, triple in enumerate(test_spo):
+        s, p, o = [triple[i].item() for i in range(3)]
+
         rel_idx = all_spo[:, 1] == p
         ent_idx = (
             (all_spo[:, 0] == s) & (all_spo[:, 2] != o)
@@ -125,37 +124,36 @@ def evaluate_rankings(scores, test_spo, all_spo, direction="o", k=10):
     :param k: k for hits@k
     :return mrr, hits: MRR and Hits@k scores for this subset of triples
     """
+    # remove true triples with same head/relation or relation/tail
     scores = filter_false_negatives(scores, test_spo, all_spo, direction=direction)
     scores[torch.isnan(scores)] = float("-Inf")
 
-    mrr, hits = [], []
+    # get the scores of the true target subjects/objects
+    idx = 0 if direction == "s" else 2
+    targets = test_spo[:, idx].long()
+    arange = torch.arange(len(targets), dtype=torch.long, device="cpu")
+    true_scores = scores[arange, targets].view(-1, 1)
 
-    for i, (s, p, o) in enumerate(test_spo):
-        row = scores[i]  # corresponding predictions
-        idx = o if direction == "o" else s
-        true_score = row[idx]
+    # remove the true subjects/objects from the scores so they don't factor in rankings
+    scores = scores.clone()
+    scores[arange, targets] = float("-Inf")
 
-        # remove current label from scores
-        row = row.clone()
-        row[idx] = float("-Inf")
+    # follow LibKGE protocol by taking the mean rank among all entities with same score
+    ranks = torch.sum(scores > true_scores, dim=1, dtype=torch.double)
+    num_ties = torch.sum(scores == true_scores, dim=1, dtype=torch.double)
+    ranks = ranks + num_ties // 2 + 1  # ranks are one-indexed
 
-        # follow LibKGE protocol for ranking and ties
-        rank = int(torch.sum(row > true_score, dtype=torch.long))
-        num_ties = int(torch.sum(row == true_score, dtype=torch.long))
-        rank = rank + num_ties // 2 + 1
-
-        # compute MRR and Hits@k
-        mrr.append(1 / rank)
-        hits.append(int(rank <= k))
-
-    return mrr, hits
+    mrr = (1 / ranks).numpy()
+    hits = (ranks <= k).numpy()
+    return list(mrr), list(hits)
 
 
+@torch.no_grad()
 def main():
     args = parse_args()
 
     # Load model checkpoint and data
-    checkpoint = load_checkpoint(args.model_checkpoint)
+    checkpoint = load_checkpoint(args.model_checkpoint, device="cpu")
     model_pt = kge.model.KgeModel.create_from(checkpoint)
     print("Loaded model from", args.model_checkpoint)
 
@@ -167,57 +165,55 @@ def main():
         dataset.split("valid"),
         dataset.split("test"),
     )
-    all_spo = torch.cat(
-        (train_spo, valid_spo, test_spo), axis=0).type(torch.LongTensor)  # for filtering
+    all_spo = torch.cat((train_spo, valid_spo, test_spo), axis=0).long()
 
     # Load relation ID to string mapping
-    relation_ids = list(dataset.relation_ids())
+    relation_ids = dataset.relation_ids()
     metric_names = ("mrr", "hits@10")
     metrics_all = defaultdict(lambda: defaultdict(list))
     dfs = []
 
     # Keep track of percentage of test triples per relation type
-    with torch.no_grad():
-        for rid in tqdm(torch.unique(test_spo[:, 1]), desc="Relation"):
-            # Get all test triples with this relation
-            test_filt = test_spo[test_spo[:, 1] == rid]
+    for rid in tqdm(torch.unique(test_spo[:, 1]), desc="Relation"):
+        rid = rid.item()
 
-            for direction in ["s", "o"]:  # (?, r, t) and (h, r, ?)
-                metrics_mean = defaultdict(dict)
+        # Get all test triples with this relation
+        test_filt = test_spo[test_spo[:, 1] == rid]
 
-                for modelname, score_fn in zip(
-                    ["Model", "Baseline"], [score_with_model, score_by_frequency]
-                ):
+        for direction in ["s", "o"]:  # (?, r, t) and (h, r, ?)
+            metrics_mean = defaultdict(dict)
 
-                    # score test triples and evaluate rankings
-                    scores = score_fn(model_pt, test_filt, direction=direction)
-                    model_metrics = evaluate_rankings(
-                        scores, test_filt, all_spo, direction=direction
-                    )
+            for modelname, score_fn in zip(
+                ["Model", "Baseline"], [score_with_model, score_by_frequency]
+            ):
 
-                    for metric_name, metric in zip(metric_names, model_metrics):
-                        metrics_mean[modelname][metric_name] = np.mean(metric)
-                        metrics_all[modelname][metric_name].extend(metric)
+                # score test triples and evaluate rankings
+                scores = score_fn(model_pt, test_filt, direction=direction)
+                model_metrics = evaluate_rankings(
+                    scores, test_filt, all_spo, direction=direction
+                )
 
-                for metric_name in metric_names:
-                    model_metric = metrics_mean["Model"][metric_name]
-                    baseline_metric = metrics_mean["Baseline"][metric_name]
-                    diff = model_metric - baseline_metric
+                for metric_name, metric in zip(metric_names, model_metrics):
+                    metrics_mean[modelname][metric_name] = np.mean(metric)
+                    metrics_all[modelname][metric_name].extend(metric)
 
-                    line = {
-                        "relation": relation_ids[int(rid)],
-                        "metric": metric_name,
-                        "direction": direction,
-                        "count": len(test_filt),
-                        "diff": diff,
-                        "model": model_metric,
-                        "baseline": baseline_metric,
-                    }
+            for metric_name in metric_names:
+                model_metric = metrics_mean["Model"][metric_name]
+                baseline_metric = metrics_mean["Baseline"][metric_name]
+                diff = model_metric - baseline_metric
 
-                    if args.csv is not None:
-                        dfs.append(
-                            pd.DataFrame.from_dict(line, orient="index").transpose()
-                        )
+                line = {
+                    "relation": relation_ids[rid],
+                    "metric": metric_name,
+                    "direction": direction,
+                    "count": len(test_filt),
+                    "diff": diff,
+                    "model": model_metric,
+                    "baseline": baseline_metric,
+                }
+
+                if args.csv is not None:
+                    dfs.append(pd.DataFrame.from_dict(line, orient="index").transpose())
 
     if args.csv is not None:
         df = pd.concat(dfs)
