b97adcf544504f548590c917c570d01e528a03a8
Tara Safavi <tsafavi@umich.edu>
2020-09-21 14:09:04 +0000

    Reproduce LP experiments

---
+++ scripts/lp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+declare -a sizes=("s" "m" "l")
+declare -a models=("rescal" "transe" "complex" "conve" "tucker")
+
+for size in "${sizes[@]}"; do
+    # Download pretrained model if not already existing
+    python download_pretrained.py ${size} link-prediction ${models[@]}
+
+    for model in "${models[@]}"; do
+        dir="models/link-prediction/codex-${size}/${model}"
+        kge test ${dir}
+    done
+done
